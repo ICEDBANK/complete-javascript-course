b1d0cd32e0ec4e48641d7107fac5c641a1f6d037
ICEDBANK <j.e.rice4101@gmail.com>
2024-05-08 14:34:29 -0400

    Automated commit

--- src/01-Fundamentals-Part-1/starter/app.js
+++ src/01-Fundamentals-Part-1/starter/app.js
@@ -163,7 +163,13 @@ console.log(String(23),23);
 console.log('I am ' + 23 + ' years old'); // console.log is making a number a string
 console.log ('23' - '10' - '3'); // console.log is making a string into a number
 
-let n = '1' + 1;
-n = n - 1;
+let n = '1' + 1; // = 11
+n = n - 1;  // = 10
 console.log(n);
+
+// 5 Falsey Value : 0, '', undefined, null, Nan
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('test'));
+console.log(Boolean({}));
 
