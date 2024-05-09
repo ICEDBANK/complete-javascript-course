a5e13c29664a0e484932f6b29d6e0827da19120a
Joshua E. Rice <j.e.rice4101@gmail.com>
2024-05-08 22:34:02 -0400

    Add files via upload

---
+++ src/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,76 @@
+// Activating Strict Mode
+
+
+
+// Functions
+
+
+
+// Function Declarations vs. Expressions
+
+
+
+// Arrow Functions
+
+
+
+// Functions Calling Other Functions
+
+
+
+// Review Functions
+
+
+
+// Coding Excercis 5 Challenge #1
+
+
+
+// Intro Into Arrays
+
+
+
+// Basic Array Operations ( Methods )
+
+
+
+// Coding Excercis 6 Challenge #2
+
+
+
+// Intro Into Objects
+
+
+
+// Dot vs. Bracket Notation
+
+
+
+// Object Methods
+
+
+
+// Coding Excercis 7 Challenge #3
+
+
+
+// Iteration: The For Loop
+
+
+
+// Looping Arrays, Breaking and Continuing
+
+
+
+// Looping Backwards and Loops In Loops
+
+
+
+// The While Loop
+
+
+
+// Coding Excercis 8 Challenge #4
+
+
+
