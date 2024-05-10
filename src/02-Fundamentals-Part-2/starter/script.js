04f7f6e61f1eb54b8a3262138341a9e784348a02
ICEDBANK <j.e.rice4101@gmail.com>
2024-05-09 22:23:28 -0400

    Automated commit

--- src/02-Fundamentals-Part-2/starter/script.js
+++ src/02-Fundamentals-Part-2/starter/script.js
@@ -322,7 +322,7 @@
         const bills = [125,555,44];
 
         const tips = [calcTip(bills[0]),calcTip(bills[1]),calcTip(bills[2])];
-        const totals = []
+        const totals = [bill[0] + tips[0], bill[0] + tips[0], bill[2] + tips[2]]
         console.log(bills, tips)
 
     //*/
