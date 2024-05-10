19f5726c454ce98655da90b5fb425b072aec53b2
ICEDBANK <j.e.rice4101@gmail.com>
2024-05-09 22:02:40 -0400

    Automated commit

--- src/02-Fundamentals-Part-2/starter/script.js
+++ src/02-Fundamentals-Part-2/starter/script.js
@@ -295,8 +295,8 @@
     function calcTip(bill){
     
         const bill = 430;
-        const tips = []
-        const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+        const tips = [.15, .2]
+        const tip = bill <= 300 && bill >= 50 ? bill * tips[0] : bill * tips[1];
         
         
         return bill;
