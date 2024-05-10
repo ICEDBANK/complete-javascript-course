d1c6870de2f2b1bb3dc0def304b06b818e05a6d6
ICEDBANK <j.e.rice4101@gmail.com>
2024-05-09 22:17:23 -0400

    Automated commit

--- src/02-Fundamentals-Part-2/starter/script.js
+++ src/02-Fundamentals-Part-2/starter/script.js
@@ -311,11 +311,17 @@
 
     ///*
 
-        const calcTip = function(bill) {
+        // const calcTip = function(bill) {
 
-            return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+        //     return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 
-        }
+        // }
+
+        const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+        const bills = [125,555,44];
+
+        const tip = [calcTip()]
 
     //*/
 
