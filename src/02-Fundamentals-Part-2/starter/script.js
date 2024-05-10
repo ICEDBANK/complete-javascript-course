ded47ea74696b8d63a03962cbb1163450cffe716
ICEDBANK <j.e.rice4101@gmail.com>
2024-05-09 21:32:08 -0400

    Automated commit

--- src/02-Fundamentals-Part-2/starter/script.js
+++ src/02-Fundamentals-Part-2/starter/script.js
@@ -257,11 +257,11 @@
 
             const friendsArray = ['Austin', 'Sarah', 'Ducky'];
             
-            const newLength = friendsArray.push('Keith'); // push function will "push" the value to the end of an array, it also return the new length
+            let newLength = friendsArray.push('Keith'); // push function will "push" the value to the end of an array, it also return the new length
             console.log(friendsArray);
             console.log(newLength);
 
-            friendsArray.unshift('John');
+            newLength = friendsArray.unshift('John');
             console.log(friendsArray);
             console.log(newLength);
 
