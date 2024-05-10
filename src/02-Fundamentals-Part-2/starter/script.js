09964165e6e6a025bd0ea69d580b6a2472b5b8af
ICEDBANK <j.e.rice4101@gmail.com>
2024-05-09 23:51:37 -0400

    Automated commit

--- src/02-Fundamentals-Part-2/starter/script.js
+++ src/02-Fundamentals-Part-2/starter/script.js
@@ -372,7 +372,7 @@
         console.log(joshua[`last` + namekey]); // Accessing the property "lastName" of the 'joshua' object by concatenating the string "last" with the value of the variable 'namekey'
         //console.loger(joshua.'last' + nameKey);
 
-        
+        // When we need to compute 
 
    //*/
 
