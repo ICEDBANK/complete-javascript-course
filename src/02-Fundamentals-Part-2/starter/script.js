cc730ef53a146258ddaea324bd690886110b1bc4
ICEDBANK <j.e.rice4101@gmail.com>
2024-05-21 23:06:00 -0400

    Automated commit

--- src/02-Fundamentals-Part-2/starter/script.js
+++ src/02-Fundamentals-Part-2/starter/script.js
@@ -601,7 +601,7 @@ const joshua =
 
 // The While Loop
                     
-                    ///*
+                    /*
 
                         // for (let rep = 1; rep <= 10; rep++) {
 
@@ -629,9 +629,31 @@ const joshua =
 
                         }
 
-                    //*/
+                    */
 
 // Coding Excercis 8 Challenge #4
 
-
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+  }
+  
+  /* Write your code below. Good luck! 🙂 */
+  
+  const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+  const tips = [];
+  const totals = [];
+  
+      for ( let i = 0; i <= bills.length; i++ ){
+          
+          const tip = calcTip(bills[i]);
+          tips.push(tip);
+          totals.push(bill[i] + tip);
+          console.log(totals);
+      }
+  
+//  const calcAverage(arr){
+      
+      
+      
+ // }
 
