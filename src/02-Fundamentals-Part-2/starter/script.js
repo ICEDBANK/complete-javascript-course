403cf79725bad7b471b86e741180133e542f39c5
ICEDBANK <j.e.rice4101@gmail.com>
2024-05-21 22:43:58 -0400

    Automated commit

--- src/02-Fundamentals-Part-2/starter/script.js
+++ src/02-Fundamentals-Part-2/starter/script.js
@@ -625,6 +625,8 @@ const joshua =
                             console.log(`You Rolled a ${dice}`);
                             dice = Math.trunc(Math.random() * 6) +1 ;
 
+                            if(dice === 6) console.log(`Loop is about to end...`);
+
                         }
 
                     //*/
