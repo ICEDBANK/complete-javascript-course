3ff701ac835683a26a085796ba715fbe91e67bab
ICEDBANK <j.e.rice4101@gmail.com>
2024-05-24 23:40:09 -0400

    Automated commit

--- src/03-Developer-Skills/starter/script.js
+++ src/03-Developer-Skills/starter/script.js
@@ -181,10 +181,10 @@ const testData1 = [17, 21, 23];
 const testData2 = [12, 5, -5, 0, 4];
 
 const printForcast = function (arr) {
-  let foreCastString = '...';
+  let foreCastString = '... ';
 
   for (let i = 0; i < arr.length; i++) {
-    foreCastString += `${arr[i]}ºC in ${arr[i + 1]} days ... `;
+    foreCastString += `${arr[i]}ºC in ${i + 1} days ... `;
   }
 
   console.log(foreCastString);
