7929dcd0ced8e3bf9b7c40b23a4b2430a30b97e9
ICEDBANK <j.e.rice4101@gmail.com>
2024-05-25 02:38:00 -0400

    Automated commit

--- src/03-Developer-Skills/starter/script.js
+++ src/03-Developer-Skills/starter/script.js
@@ -192,3 +192,47 @@ const testData2 = [12, 5, -5, 0, 4];
 
 printForcast(testData1);
 printForcast(testData2);
+
+const mark = {
+  fullName: 'Mark Miller',
+
+  mass: 78,
+
+  height: 1.69,
+
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+
+    return this.bmi;
+  },
+};
+
+const john = {
+  fullName: 'John Smith',
+
+  mass: 92,
+
+  height: 1.95,
+
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+
+    return this.bmi;
+  },
+};
+
+mark.calcBMI();
+
+john.calcBMI();
+
+console.log(mark.bmi, john.bmi);
+
+if (mark.bmi > john.bmi) {
+  console.log(
+    `${mark.fullName}'s BMI(${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})!`
+  );
+} else if (john.bmi > mark.bmi) {
+  console.log(
+    `${john.fullName}'s BMI(${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})!`
+  );
+}
