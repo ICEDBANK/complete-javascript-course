d6c4927da3a669cac34f6a6a408879e462f056b9
ICEDBANK <j.e.rice4101@gmail.com>
2024-05-24 22:44:03 -0400

    Automated commit

--- src/03-Developer-Skills/starter/script.js
+++ src/03-Developer-Skills/starter/script.js
@@ -115,10 +115,11 @@ const measureKelvin = function () {
     unit: 'cels',
     value: prompt('Degrees Celsius: '),
   };
+  console.log(measurement);
 
   console.log(measurement.value);
-  console.warn(measurement.value);
-  console.error(measurement.value);
+  //   console.warn(measurement.value);
+  //   console.error(measurement.value);
 
   const kelvin = measurement.value + 273;
   return kelvin;
