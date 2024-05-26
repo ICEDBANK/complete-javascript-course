1b91a6dcefbeea1742a788d77c592acf3b2c3b85
ICEDBANK <j.e.rice4101@gmail.com>
2024-05-25 21:43:34 -0400

    Automated commit

--- src/05-Guess-My-Number/starter/script.js
+++ src/05-Guess-My-Number/starter/script.js
@@ -5,3 +5,6 @@ console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'Correct Number!';
 
 console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent;
+document.querySelector('.score').textContent;
