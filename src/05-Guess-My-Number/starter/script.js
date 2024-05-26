38287e989cce262c2f04176036c8595ee1442363
ICEDBANK <j.e.rice4101@gmail.com>
2024-05-26 00:53:21 -0400

    Automated commit

--- src/05-Guess-My-Number/starter/script.js
+++ src/05-Guess-My-Number/starter/script.js
@@ -14,12 +14,14 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
-'use strict';
-
 const MAX_SCORE = 20;
 const MAX_NUMBER = 20;
+const MIN_NUMBER = 1;
 
-const secretNumber = Math.trunc(Math.random() * MAX_NUMBER) + 1;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(`Secret Number : ${secretNumber}`);
+document.querySelector('.number').textContent = '?';
+
 let score = MAX_SCORE;
 
 const setMessage = message => {
@@ -30,25 +32,25 @@ const updateScore = () => {
   document.querySelector('.score').textContent = score;
 };
 
-document.querySelector('.number').textContent = '?'; // Hide the secret number initially
-
 document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
+  let guess = Number(document.querySelector('.guess').value);
 
   if (!guess) {
     setMessage('No Number');
-  } else if (guess > MAX_NUMBER || guess < 1) {
-    setMessage('Enter a Number between 1 and 20');
+  } else if (guess > MAX_NUMBER || guess < MIN_NUMBER) {
+    setMessage('Enter A number Between 1 and 20');
+    score--;
+    updateScore();
   } else if (guess === secretNumber) {
-    setMessage('Correct Number');
+    setMessage('Correct');
     document.querySelector('.number').textContent = secretNumber;
   } else {
     if (score > 1) {
-      setMessage(guess < secretNumber ? 'Too Low' : 'Too High');
+      setMessage(guess < secretNumber ? 'To Low' : 'To High');
       score--;
       updateScore();
     } else {
-      setMessage('You lost the game!');
+      setMessage('You Lost Click The "Again" Button to try again');
       score = 0;
       updateScore();
     }
