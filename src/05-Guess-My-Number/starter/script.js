25ea8516ae8a9da2646e8cabbe5237db88d177a3
ICEDBANK <j.e.rice4101@gmail.com>
2024-05-25 23:57:02 -0400

    Automated commit

--- src/05-Guess-My-Number/starter/script.js
+++ src/05-Guess-My-Number/starter/script.js
@@ -17,12 +17,16 @@ console.log(document.querySelector('.guess').value);
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.number').textContent = secretNumber;
 
+const setMessage = message => {
+  document.querySelector('.message').textContent = message;
+};
+
 document.querySelector('.check').addEventListener('click', function () {
   let guess = Number(document.querySelector('.guess').value);
   let message = document.querySelector('.message').textContent;
 
   if (!guess) {
-    message = 'Nu Number';
+    setmessage('Nu Number');
   } else if (guess > 20 || guess < 1) {
     message = 'Enter a Number between 1 and 20';
   } else if (guess === secretNumber) {
