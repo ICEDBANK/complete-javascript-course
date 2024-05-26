a4e994c6698b1bf1a7179335fcd14a93ca52b5c7
ICEDBANK <j.e.rice4101@gmail.com>
2024-05-25 22:15:26 -0400

    Automated commit

--- src/05-Guess-My-Number/starter/script.js
+++ src/05-Guess-My-Number/starter/script.js
@@ -14,13 +14,15 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
-const number = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = number;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   let guess = Number(document.querySelector('.guess').value);
 
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number';
   }
 });
