65b2163b764157ce49184e940b6b489873a19ac1
ICEDBANK <j.e.rice4101@gmail.com>
2024-05-26 23:09:25 -0400

    Automated commit

--- src/06-Modal/starter/script.js
+++ src/06-Modal/starter/script.js
@@ -7,35 +7,17 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
 
 // Function to open the modal
-const closeModal = function () {
-  modalEl.classList.add('hidden');
-  modelOverlayEl.classList.add('hidden');
-};
 
 // Function to open the modal
-const openModal = function () {
-  modalEl.classList.remove('hidden');
-  modelOverlayEl.classList.remove('hidden');
-};
 
 // Adding event listeners to open the modal when buttons are clicked
-btnOpenModal.forEach(button => {
-  button.addEventListener('click', openModal);
-});
 
 // Adding event listener to close the modal when the close button is clicked
-btnCloseModal.addEventListener('click', closeModal);
 
 // Adding event listener to close the modal when the overlay is clicked
-modelOverlayEl.addEventListener('click', closeModal);
 
 // Adding event listener to detect keydown events on the whole document
-document.addEventListener('keydown', function (event) {
-  console.log(event.key);
 
-  // Checking if the pressed key is the 'Escape' key and if the modal is not hidden
-  if (event.key === 'Escape' && !modalEl.classList.contains('hidden')) {
-    // If conditions are met, call the closeModal function to close the modal
-    closeModal();
-  }
-});
+// Checking if the pressed key is the 'Escape' key and if the modal is not hidden
+
+// If conditions are met, call the closeModal function to close the modal
