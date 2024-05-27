eadd880eb3d3f8addc785c8ab67f3733116c5a49
ICEDBANK <j.e.rice4101@gmail.com>
2024-05-26 22:38:01 -0400

    Automated commit

--- src/06-Modal/starter/script.js
+++ src/06-Modal/starter/script.js
@@ -31,9 +31,7 @@ modelOverlayEl.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', function (event) {
   console.log(event.key);
-  if (event.key === 'Escape') {
-    if (!modalEl.classList.contains('hidden')) {
-      closeModal();
-    }
+  if (event.key === 'Escape' && !modalEl.classList.contains('hidden')) {
+    closeModal();
   }
 });
