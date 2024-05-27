8607c09bf168661306b50ede0dbdc587ef4b7ead
ICEDBANK <j.e.rice4101@gmail.com>
2024-05-26 22:42:57 -0400

    Automated commit

--- src/06-Modal/starter/script.js
+++ src/06-Modal/starter/script.js
@@ -7,31 +7,14 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
 
 // Function to close the modal
-const closeModal = function () {
-  modalEl.classList.add('hidden');
-  modelOverlayEl.classList.add('hidden');
-};
+const closeModal = function () {};
 
 // Function to open the modal
-const openModal = function () {
-  modalEl.classList.remove('hidden');
-  modelOverlayEl.classList.remove('hidden');
-};
 
 // Adding event listeners to open the modal when buttons are clicked
-btnOpenModal.forEach(button => {
-  button.addEventListener('click', openModal);
-});
 
 // Adding event listener to close the modal when the close button is clicked
-btnCloseModal.addEventListener('click', closeModal);
 
 // Adding event listener to close the modal when the overlay is clicked
-modelOverlayEl.addEventListener('click', closeModal);
 
-document.addEventListener('keydown', function (event) {
-  console.log(event.key);
-  if (event.key === 'Escape' && !modalEl.classList.contains('hidden')) {
-    closeModal();
-  }
-});
+// Adding event listener to detect keydown events on the whole document
