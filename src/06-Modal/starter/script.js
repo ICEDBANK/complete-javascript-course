a9d16defacb78f8aa5c117fd88e7d702d644d605
ICEDBANK <j.e.rice4101@gmail.com>
2024-05-26 23:04:31 -0400

    Automated commit

--- src/06-Modal/starter/script.js
+++ src/06-Modal/starter/script.js
@@ -7,10 +7,10 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
 
 // Function to close the modal
-// const closeModal = function () {
-//   modalEl.classList.add('hidden');
-//   modelOverlayEl.classList.add('hidden');
-// };
+const closeModal = function () {
+  modalEl.classList.add('hidden');
+  modelOverlayEl.classList.add('hidden');
+};
 
 // Function to open the modal
 const openModal = function () {
@@ -25,7 +25,7 @@ btnOpenModal.forEach(button => {
 });
 
 // Adding event listener to close the modal when the close button is clicked
-btnCloseModal.addEventListener('click', closeModal);
+btnCloseModal.addEventListener('click', closeModal(){console.log('closed');});
 
 // Adding event listener to close the modal when the overlay is clicked
 document.addEventListener('click', function () {
