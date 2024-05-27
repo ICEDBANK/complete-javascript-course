7972490ca686324adf4378576080f48b5c75089f
ICEDBANK <j.e.rice4101@gmail.com>
2024-05-27 02:01:51 -0400

    Automated commit

--- src/07-Pig-Game/starter/script.js
+++ src/07-Pig-Game/starter/script.js
@@ -72,11 +72,14 @@
 'use strict';
 
 //  The system generates a random dice roll value between 1 and 6.
-const diceEl = document.querySelector('.dice');
 const MAX_SCORE = 100;
 
-let score0El = (document.querySelector('#score--0').textContent = 0);
-let score1El = (document.getElementById('score--1').textContent = 0);
+let score0El = document.querySelector('#score--0');
+let score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
 let diceRollRandom = function () {};
