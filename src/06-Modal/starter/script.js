3641859e2152d039bfa8353f6e7af448c8461511
ICEDBANK <j.e.rice4101@gmail.com>
2024-05-26 22:59:07 -0400

    Automated commit

--- src/06-Modal/starter/script.js
+++ src/06-Modal/starter/script.js
@@ -14,6 +14,7 @@ const closeModal = function () {
 
 // Function to open the modal
 const openModal = function () {
+  console.log(btnOpenModal);
   modalEl.classList.remove('hidden');
   modelOverlayEl.classList.remove('hidden');
 };
