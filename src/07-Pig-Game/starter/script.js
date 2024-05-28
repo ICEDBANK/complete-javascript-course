fae1f27c950e3b40cdacda84d709d8d110d9c2f1
ICEDBANK <j.e.rice4101@gmail.com>
2024-05-28 00:57:27 -0400

    Automated commit

--- src/07-Pig-Game/starter/script.js
+++ src/07-Pig-Game/starter/script.js
@@ -95,7 +95,9 @@ const init = function () {
   current0El.textContent = 0;
   current1El.textContent = 0;
   player0El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
   player1El.classList.remove('player--winner');
+  player1El.classList.remove('player--active');
   score = [0, 0];
   currentScore = 0;
   activePlayer = 0;
