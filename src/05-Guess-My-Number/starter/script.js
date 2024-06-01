63964795e174bdf3d818f13023ee5eb8451477af
ICEDBANK <j.e.rice4101@gmail.com>
2024-05-31 21:24:56 -0400

    Automated commit

--- src/05-Guess-My-Number/starter/script.js
+++ src/05-Guess-My-Number/starter/script.js
@@ -61,5 +61,5 @@ const highscore = document.querySelector('.highscore');
 btnCheck.addEventListener('click', function () {
   let guess = 0;
   guess = Number(document.querySelector('.guess').value);
-  number.value;
+  console.log(number.value);
 });
