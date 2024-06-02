9aae580dd6f9e5a714b5d07783136f30b0b5da9e
ICEDBANK <j.e.rice4101@gmail.com>
2024-06-01 22:13:15 -0400

    Automated commit

--- src/08-Behind-the-Scenes/starter/script.js
+++ src/08-Behind-the-Scenes/starter/script.js
@@ -148,7 +148,14 @@
 
 function calcAge(birthYear) {
   const age = 2024 - birthYear;
-  console.log(firstName);
+
+  function printAge() {
+    const output = `You are ${age}, born in ${birthYear}`;
+    console.log(output);
+  }
+
+  printAge();
+
   return age;
 }
 
