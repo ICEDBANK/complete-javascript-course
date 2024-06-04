00c03cc1a28b18cdcb007df497b0b70e5b4c68b4
ICEDBANK <j.e.rice4101@gmail.com>
2024-06-03 23:39:21 -0400

    Automated commit

--- src/09-Data-Structures-Operators/starter/codingChallenge-1.js
+++ src/09-Data-Structures-Operators/starter/codingChallenge-1.js
@@ -73,7 +73,7 @@ const game = {
 };
 
 //1. Create one player array for each team (variables 'players1' and 'players2')
-let {
+const {
   players: [players1, players2],
 } = game;
 
@@ -85,10 +85,14 @@ const [goalKeeper2, ...fieldPlayers2] = players2;
 const allPlayers = [...players1, ...players2];
 
 // 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
-const players1Final = ['Thiago', 'Coutinho', 'Perisic', ...players1];
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
 
 // 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
-const { team1, x: draw, team2 } = game.odds;
+//const { team1, x: draw, team2 } = game.odds;
+const {
+  odds: { team1, draw, team2 },
+} = game;
+console.log(team1, draw, team2);
 
 // 6b. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
 game.printGoals('Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels');
