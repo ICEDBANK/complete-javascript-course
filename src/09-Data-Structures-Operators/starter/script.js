89b5470290fe1394eebb109935b1aaf2313ea01b
ICEDBANK <j.e.rice4101@gmail.com>
2024-06-05 22:58:34 -0400

    Automated commit

--- src/09-Data-Structures-Operators/starter/script.js
+++ src/09-Data-Structures-Operators/starter/script.js
@@ -1081,7 +1081,7 @@ for (const order of ordersSet) console.log(order);
 
 //Example
 const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
-const staffUnique = [...new Set(staffUnique)];
+const staffUnique = [...new Set(staff)];
 console.log(staffUnique);
 console.log(
   new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size
