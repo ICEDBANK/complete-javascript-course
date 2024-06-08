eb8237d4a447371f58cf4db609367fd6f7784f27
ICEDBANK <j.e.rice4101@gmail.com>
2024-06-08 01:52:39 -0400

    Automated commit

--- src/09-Data-Structures-Operators/starter/codingChallenge-1.js
+++ src/09-Data-Structures-Operators/starter/codingChallenge-1.js
@@ -241,7 +241,7 @@ const stringArray = [];
 
 const camelCase = function (str) {
   const [s1, s2] = str.split('_');
-  s2[0].toUpper();
+  s2[1].toUpperCase();
   console.log([s1, s2]);
 };
 camelCase(string1);
