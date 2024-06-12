4e65ffae5e005a39e366a72e234dcfb2601845a2
ICEDBANK <j.e.rice4101@gmail.com>
2024-06-11 22:27:55 -0400

    Automated commit

--- src/10-Functions/starter/script.js
+++ src/10-Functions/starter/script.js
@@ -38,31 +38,10 @@ const poll = {
   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
   // This generates [0, 0, 0, 0]. More in the next section 😃
   answers: new Array(4).fill(0),
-  registerNewAnswer(question, options) {
-    const questionString = `${this.question}\n${this.options.join(
-      '\n'
-    )}\n(Write option number)`;
-    const answer = prompt(questionString);
-    const answerNumber = parseInt(answer);
-
-    if (
-      isNaN(answerNumber) ||
-      answerNumber < 0 ||
-      answerNumber > answer.length
-    ) {
-      console.log(`${answerNumber} is Not Valid`);
-    } else {
-      this.answers[answerNumber]++;
-      this.displayResults();
-    }
-  },
-  displayResults() {
-    console.log(`Poll Results: ${this.answers}`);
+  registerNewAnswer() {
+    const outputString = prompt(`${this.question}`);
   },
 };
-document
-  .querySelector('.poll')
-  .addEventListener('click', poll.registerNewAnswer.bind(poll));
 
 // Overview of the bind Method in JavaScript
 
