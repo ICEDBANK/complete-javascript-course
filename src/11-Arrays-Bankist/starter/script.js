696660b38b0c6a60f2552a14447cb1f70057661f
ICEDBANK <j.e.rice4101@gmail.com>
2024-06-13 22:06:15 -0400

    Automated commit

--- src/11-Arrays-Bankist/starter/script.js
+++ src/11-Arrays-Bankist/starter/script.js
@@ -86,6 +86,8 @@ console.log(arr.slice());
 console.log(...arr);
 
 // Splice Array Method
-console.log(arr.splice(2));
+//console.log(arr.splice(2));
 arr.splice(-1);
+console.log(arr);
+arr.splice(1, 2);
 console.log(arr);
