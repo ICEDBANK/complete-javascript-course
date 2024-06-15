439b7390e8f9b11d044a8ea2d809bd4c4d3905fb
ICEDBANK <j.e.rice4101@gmail.com>
2024-06-14 23:59:25 -0400

    Automated commit

--- src/11-Arrays-Bankist/starter/script.js
+++ src/11-Arrays-Bankist/starter/script.js
@@ -153,9 +153,4 @@ rice.forEach(function (value, key) {
   console.log(`The Rice Family consist of ${key} at age ${value}`);
 });
 
-let a = 1;
-let b = 2;
-
-[a, b] = [b, a];
-
-console.log(a, b);
+console.log('5' - 3);
