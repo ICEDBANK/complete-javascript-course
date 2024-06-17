f47263988cd3454c19cf413f72f24f12d4d3b90b
ICEDBANK <j.e.rice4101@gmail.com>
2024-06-16 22:07:30 -0400

    Automated commit

--- src/11-Arrays-Bankist/starter/script.js
+++ src/11-Arrays-Bankist/starter/script.js
@@ -85,6 +85,7 @@ displayMovements(account1.movements);
 const verifyLogin = function (user, pin) {
   user = inputLoginUsername.innerHTML;
   pin = inputLoginPin;
+  console.log(`${user} , ${pin}`);
 };
 
 btnLogin.addEventListener('click', verifyLogin);
