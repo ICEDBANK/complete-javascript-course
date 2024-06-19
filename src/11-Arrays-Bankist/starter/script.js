01653c74a0415fd87b34e8827b119644c39cd93a
ICEDBANK <j.e.rice4101@gmail.com>
2024-06-18 22:35:13 -0400

    Automated commit

--- src/11-Arrays-Bankist/starter/script.js
+++ src/11-Arrays-Bankist/starter/script.js
@@ -436,8 +436,8 @@ const testData2 = [16, 6, 10, 5, 6, 1, 4];
 const calcAverageHumanAge = function (ages) {
   const humanYears = ages.map(dog => (dog <= 2 ? 2 * dog : dog * 4 + 16));
   console.log(`Human Years ${humanYears}`);
-  const dogFilter = humanYears.filter(dog => dog <= 18);
-  console.log(dogFilter);
+  const dogFilter = humanYears.filter(dog => dog >= 18);
+  console.log(`Filtered Years ${dogFilter}`);
   if (dogFilter.length > 0) {
     const average = dogFilter.reduce(
       (acc, curr) => acc + curr / dogFilter.length
