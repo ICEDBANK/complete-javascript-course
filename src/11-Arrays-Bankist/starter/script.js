0ce89570f95caf1c33403bb764915bbab54058c9
ICEDBANK <j.e.rice4101@gmail.com>
2024-06-21 23:05:45 -0400

    Automated commit

--- src/11-Arrays-Bankist/starter/script.js
+++ src/11-Arrays-Bankist/starter/script.js
@@ -199,7 +199,7 @@ btnClose.addEventListener('click', function (e) {
     const index = accounts.findIndex(
       acc => acc.username === currentAccount.username
     );
-    console.log(`You have deleted ${accounts}`);
+    console.log(`${index}`);
     accounts.splice(index, 1);
 
     // containerApp.style.opacity = 0;
