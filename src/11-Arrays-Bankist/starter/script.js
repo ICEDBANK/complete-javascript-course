462058113937ac33742d5e83bbaad18e455a98db
ICEDBANK <j.e.rice4101@gmail.com>
2024-06-22 22:07:57 -0400

    Automated commit

--- src/11-Arrays-Bankist/starter/script.js
+++ src/11-Arrays-Bankist/starter/script.js
@@ -193,7 +193,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  let amount = Number(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => amount * 0.1)) {
     // Add Movement
