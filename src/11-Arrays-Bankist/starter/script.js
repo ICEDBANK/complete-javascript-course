4a48bf77be556d8c8af49412183cc9bd7a2f4c9a
ICEDBANK <j.e.rice4101@gmail.com>
2024-06-24 22:00:46 -0400

    Automated commit

--- src/11-Arrays-Bankist/starter/script.js
+++ src/11-Arrays-Bankist/starter/script.js
@@ -595,7 +595,8 @@ console.log(y);
 const z = Array.from({ length: 7 }, (cur, i) => i + 1);
 console.log(z);
 
-const dice = ({ length: 100 }, (cur, i) => i + Math.trunc(Math.random() * 6));
+const dice =
+  ({ length: 100 }, (cur, i) => i + Math.trunc(Math.random(), cur * 6));
 console.log(dice);
 
 ///////////////////////////////////////
