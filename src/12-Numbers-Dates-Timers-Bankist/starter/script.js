6d1ff37879328e3c115c1ac59cd7635626768501
ICEDBANK <j.e.rice4101@gmail.com>
2024-06-25 21:59:16 -0400

    Automated commit

--- src/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ src/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -308,4 +308,6 @@ console.log(Math.min(5, 18, '23px', '11', 2));
 
 console.log(Math.PI * Number.parseFloat('10px') ** 2);
 console.log(Math.trunc(Math.random() * 6) + 1);
-const randomInt = (min, max) => Math.trunc(Math.random() * (max - min)) + 1;
+const randomInt = (min, max) =>
+  Math.trunc(Math.random() * (max - min) + 1) + min;
+console.log(randomInt(10, 20));
