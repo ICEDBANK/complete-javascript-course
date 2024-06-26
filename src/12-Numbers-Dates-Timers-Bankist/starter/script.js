87e288037889b50ba2cf0cc2b2860240776711b9
ICEDBANK <j.e.rice4101@gmail.com>
2024-06-25 23:41:44 -0400

    Automated commit

--- src/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ src/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -332,5 +332,7 @@ console.log(8 % 3);
 console.log(8 / 3);
 console.log(6 % 2);
 console.log(6 / 2);
-console.log(5 % 2);
-console.log(5 % 2);
+console.log(7 % 2);
+console.log(7 / 2);
+
+const isEven = n => n % 2 === 0;
