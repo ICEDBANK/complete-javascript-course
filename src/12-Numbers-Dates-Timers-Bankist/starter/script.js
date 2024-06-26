95473c91d1ca1d5cb035500cc6986f387d35e6f4
ICEDBANK <j.e.rice4101@gmail.com>
2024-06-25 23:40:48 -0400

    Automated commit

--- src/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ src/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -328,8 +328,9 @@ console.log(+(2.75453).toFixed(3));
 
 console.log(5 % 2);
 console.log(5 / 2);
-console.log(5 % 2);
-console.log(5 % 2);
-console.log(5 % 2);
+console.log(8 % 3);
+console.log(8 / 3);
+console.log(6 % 2);
+console.log(6 / 2);
 console.log(5 % 2);
 console.log(5 % 2);
