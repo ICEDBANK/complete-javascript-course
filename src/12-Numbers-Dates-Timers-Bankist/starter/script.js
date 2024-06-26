248b87e4204ffd7a09636ee45aa9ad910cc83065
ICEDBANK <j.e.rice4101@gmail.com>
2024-06-25 21:46:37 -0400

    Automated commit

--- src/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ src/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -284,5 +284,10 @@ console.log(+`23`);
 
 //Parsing
 
-console.log(Number.parseInt('30px'));
-console.log(Number.parseInt('e23'));
+console.log(Number.parseInt('30px', 10));
+console.log(Number.parseInt('e23', 10));
+
+console.log(Number.parseInt('2.5rem'));
+console.log(Number.parseFloat('2.5rem'));
+
+console.log(Number.isNaN(20));
