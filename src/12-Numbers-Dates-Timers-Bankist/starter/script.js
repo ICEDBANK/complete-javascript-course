32bea7c4e5ea6862d7b53a56015a259a3f930319
ICEDBANK <j.e.rice4101@gmail.com>
2024-06-26 22:19:51 -0400

    Automated commit

--- src/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ src/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -222,20 +222,20 @@ currentAccount = account1;
 updateUI(currentAccount);
 containerApp.style.opacity = 100;
 
-const now1 = new Date();
-const options = {
-  hour: 'numeric',
-  minute: 'numeric',
-  second: 'numeric',
-  month: 'long',
-  day: 'numeric',
-  year: 'numeric',
-  weekday: 'long',
-};
-const local = navigator.language;
-console.log(local);
+// const now1 = new Date();
+// const options = {
+//   hour: 'numeric',
+//   minute: 'numeric',
+//   second: 'numeric',
+//   month: 'long',
+//   day: 'numeric',
+//   year: 'numeric',
+//   weekday: 'long',
+// };
+// const local = navigator.language;
+// console.log(local);
 
-labelDate.textContent = new Intl.DateTimeFormat('en-US', options).format(now1);
+// labelDate.textContent = new Intl.DateTimeFormat('en-US', options).format(now1);
 
 btnLogin.addEventListener('click', function (e) {
   // Prevent form from submitting
